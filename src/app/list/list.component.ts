71af43ab8a59069feafe3604c82a0db2c3120daa
Samantha Mitchell <smitch41@uncc.edu>
2019-09-24 20:00:13 -0400

    third commit

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -13,7 +13,7 @@ public employeeList = [{a: 1},{b: 2}];
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get('214.14.14.13:3000/employees')
+    this.http.get('http://167.71.162.81:3000/employees')
     .subscribe(resp => {
       this.employeeList = resp.response;
     });
